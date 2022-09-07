5c49cbaaf16fe0e728d43a70be24bdcb31029cfe
Nate Subra <12102127+natesubra@users.noreply.github.com>
2022-09-06 23:04:26 -0500

    fix build script

--- build.bat
+++ build.bat
@@ -1,5 +1,4 @@
 rd /s /q .\Debug .\Release .\X64 .\_build
-mkdir _build
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\amd64\msbuild.exe" .\winapiexec.sln -target:Clean
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\amd64\msbuild.exe" .\winapiexec.sln /property:Configuration=Release /property:Platform=x64
-"C:\Program Files\Microsoft Visual Studio\2022\Professional\Msbuild\Current\Bin\amd64\msbuild.exe" .\winapiexec.sln /property:Configuration=Release /property:Platform=Win32
+msbuild.exe .\winapiexec.sln -target:Clean
+msbuild.exe .\winapiexec.sln /property:Configuration=Release /property:Platform=x64
+msbuild.exe .\winapiexec.sln /property:Configuration=Release /property:Platform=Win32
