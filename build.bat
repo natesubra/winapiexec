85d181345b0ee6151af63df4ac01cc4c2658c280
Nate Subra <12102127+natesubra@users.noreply.github.com>
2022-09-07 00:23:45 -0500

    revert build

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 rd /s /q .\Debug\ .\Release\ .\x64\
 msbuild.exe -nologo -target:Clean
-msbuild.exe -nologo -target:Clean /property:Configuration=Release /property:Platform=Win32
-msbuild.exe -nologo -target:Clean /property:Configuration=Release /property:Platform=x64
+msbuild.exe -nologo -property:Configuration=Release -property:Platform=Win32
+msbuild.exe -nologo -property:Configuration=Release -property:Platform=x64
