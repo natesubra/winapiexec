5cdeea29357cfa02b376a6c0e616108a572d0133
Nate Subra <12102127+natesubra@users.noreply.github.com>
2022-09-06 23:52:37 -0500

    cleanup

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
-rd /s /q .\Debug .\Release .\X64 .\_build
 msbuild.exe .\winapiexec.sln -target:Clean
-msbuild.exe .\winapiexec.sln /property:Configuration=Release /property:Platform=x64
 msbuild.exe .\winapiexec.sln /property:Configuration=Release /property:Platform=Win32
+msbuild.exe .\winapiexec.sln /property:Configuration=Release /property:Platform=All
